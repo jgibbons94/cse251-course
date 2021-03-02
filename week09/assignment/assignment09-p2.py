eb26282282bea39c8c61b4d27a1a995bec996b5e
Jesse Gibbons <gib19014@byui.edu>
2021-03-02 10:24:43 -0700

    Complete part 2

--- week09/assignment/assignment09-p2.py
+++ week09/assignment/assignment09-p2.py
@@ -18,12 +18,16 @@ change the program to display the found path to the end position.
 
 What would be your strategy?  
 
-<Answer here>
+When the thread reaches a fork, Use a thread pool to get the values returned from each extended path
+like in part 1. Of the values returned, if there is a non-empty list, extend the current value
+list to it and return the current value list. Otherwise return the empty list.
 
 
 Why would it work?
 
-<Answer here>
+This is exactly the approach I took in part 1, except instead of using recursion it uses threads.
+This approach will probably be slower because it explores every square the algorithm can reach and
+creates many threads, but it will return a direct path.
 
 """
 import math
@@ -73,14 +77,55 @@ def get_color():
     current_color_index += 1
     return color
 
+def maze_can_move(lock, maze, row, col):
+    with lock:
+        return maze.can_move_here(row, col)
+
+def solve_path(maze, start, color, path_found, lock):
+    """Find a path to the next dead end.
+    When a fork is reached, spawn a new thread for every path except 1,
+    and go down the remaining path.
+    Push all created threads to threads.
+    When a dead end is reached or path_found is True, die.
+    When the end is reached, set path_found to True."""
+    threads = []
+    (row, col) = start
+    while True:
+        if path_found[0] or not maze_can_move(lock, maze, row, col):
+            [thread.join() for thread in threads]
+            return
+        with lock:
+            maze.move(row, col, color)
+        moves = maze.get_possible_moves(row, col)
+        if len(moves) == 0:
+            if maze.at_end(row, col):
+                path_found[0] = True
+            [thread.join() for thread in threads]
+            return
+        elif len(moves) > 1:
+            new_threads = [threading.Thread(target = solve_path, args =
+                (maze, moves[i], get_color(), path_found, lock)) for i in range(len(moves) - 1)]
+            threads.extend(new_threads)
+            [thread.start() for thread in new_threads]
+            (row, col) = moves[-1]
+        else:
+            (row, col) = moves[0]
+
+
 
 def solve_find_end(maze):
     """ finds the end position using threads.  Nothing is returned """
-    # When one of the threads finds the end position, stop all of them
-
-
-    pass
-
+    # A bool copies its value to the next thread, but a list carries its values to all threads.
+    path_found = [False]
+    start_pos = maze.get_start_pos()
+    color = get_color()
+    lock = threading.Lock()
+    threads = []
+    start_thread = threading.Thread(target = solve_path,
+        args = (maze, start_pos, color, path_found, lock))
+    threads.append(start_thread)
+    start_thread.start()
+    start_thread.join()
 
 def find_end(log, filename, delay):
     """ Do not change this function """
